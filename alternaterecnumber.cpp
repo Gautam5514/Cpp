561479c5a5a445c95f605fd9345f3a76e13e8272
Gautam <gautampandit797@gmail.com>
2022-12-30 00:37:44 +0530

    new added

---
+++ alternaterecnumber.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int f(int n)
+{
+    // base case
+    if (n == 0)
+        return 0;
+    // assumption
+    return f(n - 1) + ((n % 2 == 0) ? (-n) : (n));
+}
+int main()
+{
+    cout << f(10) << " ";
+    return 0;
+}
