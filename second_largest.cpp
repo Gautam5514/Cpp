de13a960de2542d2f9e3a2799141c123e904e7c9
Gautam <gautampandit797@gmail.com>
2023-01-13 01:49:40 +0530

    added

--- second_largest.cpp
+++ second_largest.cpp
@@ -18,9 +18,10 @@ int largestElementIndex(int array[], int size)
 
 int main()
 {
-    int array[]= {1, 2, 4, 5, 7, 9};
+    int array[]= {1, 2, 4, 5, 10, 9};
     int indexOfLargest = largestElementIndex(array,6);
     array[indexOfLargest] = -1;
+    
 
     int secondLargestIndex = largestElementIndex(array,6);
     cout<<array[secondLargestIndex]<<endl;
