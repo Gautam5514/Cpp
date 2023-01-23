89d1a45e1505cbc3606984be90455d835fe3b666
Gautam <gautampandit797@gmail.com>
2023-01-23 15:58:40 +0530

    added

--- subsetofRecursion.cpp
+++ subsetofRecursion.cpp
@@ -1,28 +1,26 @@
-#include <iostream>
-#include <vector>
+#include<iostream>
+#include<vector>
 using namespace std;
 
-void f(int *arr, int i, int n, int sum, vector<int> &result)
+void f(int *arr, int i, int n, int sum, vector<int>&result)
 {
-    // base case
-    if (i == n)
+    if(i ==n)
     {
         result.push_back(sum);
         return;
     }
-    f(arr, i + 1, n, sum + arr[i], result);
-    f(arr, i + 1, n, sum, result);
+    f(arr, i+1, sum + arr[i], n, result );
+    f(arr, i+1, sum, n, result );
 }
-
 int main()
 {
-    int arr[] = {4, 6, 8};
-    int n = 3;
-    vector<int> result;
-    f(arr, n, 0, 0, result);
-    for (int i = 0; i < result.size(); i++)
+    int arr[] = {2,4};
+    int n = 2;
+    vector<int>result;
+    f(arr,n,0,0,result);
+    for(int i = 0; i < result.size();i++)
     {
-        cout << result[i] << " ";
+        cout<<result[i]<<" ";
     }
     return 0;
 }
