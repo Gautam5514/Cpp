ffbd65209c73b9c90936bd0317a79b70e500ce01
Gautam <gautampandit797@gmail.com>
2023-02-12 10:01:40 +0530

    added

---
+++ 0sEndofMaintain.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+void  swapZeroToEnd(vector<int> &v){
+    int n = v.size();
+
+    for(int i = n-i; i>= 0; i--){
+        int j = 0;
+        bool flag = false;
+        while(j!=i){
+            if(v[j]==0 && v[j+1]!=0){
+                swap(v[j], v[j+1]);
+                flag = true;
+            }
+            j++;
+    }
+    if(!flag) break;
+}
+return;
+}
+
+int main()
+{
+    int n;
+    cin>>n;
+    vector<int> v(n);
+    for(int i = 0; i < n; i++){
+        cin>>v[i];
+    }
+    swapZeroToEnd (v);
+    for(int i = 0; i<n;i++){
+        cout<<v[i]<<" ";
+    }
+    cout<<endl;
+}
